60628a6a74af6b2b8026a714fc4a975d6aa6528f
Ofer <oferh@lycos.com>
2016-08-10 22:25:00 +0300

    Updates

--- ng2-completer.js
+++ ng2-completer.js
@@ -487,6 +487,9 @@ return /******/ (function(modules) { // webpackBootstrap
 	        }
 	    };
 	    CompleterCmp.prototype.keydownHandler = function (event) {
+	        if (!this.listCmp) {
+	            return;
+	        }
 	        if (event.keyCode === KEY_EN && this.results) {
 	            if (this.listCmp.currentIndex >= 0 && this.listCmp.currentIndex < this.results.length) {
 	                event.preventDefault();
