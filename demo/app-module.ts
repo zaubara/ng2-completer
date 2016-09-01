3210777e9233b90349315ddaf421343eea3541c1
Jamie Bertram <jamie@novoideation.ca>
2016-08-31 20:01:46 -0600

    Removed unneccessary imports from demo app module

--- demo/app-module.ts
+++ demo/app-module.ts
@@ -1,15 +1,18 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent }  from './app-cmp';
-import { RouterModule } from "@angular/router";
 import { HttpModule } from "@angular/http";
 import { Ng2CompleterModule } from "../src/ng2-completer.module";
-import { ReactiveFormsModule, FormsModule } from "@angular/forms";
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
-  imports:      [ BrowserModule, RouterModule, HttpModule, Ng2CompleterModule, FormsModule, ReactiveFormsModule ],
+  imports: [
+      BrowserModule,
+      Ng2CompleterModule,
+      FormsModule,
+      HttpModule
+  ],
   declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
