5476464be912dda456c2d8c626b646d2e4a7d7f7
Jamie Bertram <jamie@novoideation.ca>
2016-08-31 19:48:56 -0600

    updated demo project for new architecture

--- demo/app-module.ts
+++ demo/app-module.ts
@@ -1,14 +1,15 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { provideForms } from '@angular/forms';
-import {HTTP_PROVIDERS} from "@angular/http";
 
 import { AppComponent }  from './app-cmp';
+import { RouterModule } from "@angular/router";
+import { HttpModule } from "@angular/http";
+import { Ng2CompleterModule } from "../src/ng2-completer.module";
+import { ReactiveFormsModule, FormsModule } from "@angular/forms";
 
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [ BrowserModule, RouterModule, HttpModule, Ng2CompleterModule, FormsModule, ReactiveFormsModule ],
   declarations: [ AppComponent ],
-  providers:    [HTTP_PROVIDERS, provideForms()],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
