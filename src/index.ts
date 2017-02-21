2caed6886bf25369adf2d03f8a3024ebe3974e0d
Ofer <oferh@lycos.com>
2017-02-21 08:52:06 +0200

    missing exports for ionic build

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 export {Ng2CompleterModule} from "./ng2-completer.module";
 
 export {CompleterCmp} from "./components/completer-cmp";
+export {CompleterListItemCmp} from "./components/completer-list-item-cmp";
 export {CompleterService} from "./services/completer-service";
 export {CompleterData} from "./services/completer-data";
+export {localDataFactory, LocalDataFactoryProvider, remoteDataFactory, RemoteDataFactoryProvider} from "./services/completer-data-factory";
 export {CompleterItem} from "./components/completer-item";
 export {LocalData} from "./services/local-data";
 export {RemoteData} from "./services/remote-data";
